bc80bef61e90ed95135a83e0f28b9e57adbabe10
Daniel Waterworth <da.waterworth@gmail.com>
2018-03-08 16:29:52 -0600

    Test rename

--- src/test.mjs
+++ src/test.mjs
@@ -2,7 +2,7 @@ import { Input, RValue, RStack, apply } from "./main.mjs";
 import * as view from "./main.mjs";
 import { group, driver, entry } from "./driver.mjs";
 
-driver('stackToMap#1', console => {
+driver('stackToMap:1', console => {
   let events = new Input(new RStack([]));
   events.applyChanges([{'type': 'push', 'value': new RValue(['foo', 'bar'])}]);
 
